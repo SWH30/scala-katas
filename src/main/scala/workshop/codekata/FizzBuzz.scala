adc1d6a652d1676952d68a74c02ec1b95c1b723e
Stephen Whalley <stephen.whalley@sky.uk>
2019-09-15 09:20:21 +0100

    Implement fizzbuzz

--- src/main/scala/workshop/codekata/FizzBuzz.scala
+++ src/main/scala/workshop/codekata/FizzBuzz.scala
@@ -1,5 +1,11 @@
 package workshop.codekata
 
 object FizzBuzz {
-  def getResult(i: Int) = ""
+  def getResult(i: Int): String =
+    i % 3 -> i % 5 match {
+      case (0, 0) => "fizzbuzz"
+      case (0, _) => "fizz"
+      case (_, 0) => "buzz"
+      case _ => i.toString
+    }
 }
